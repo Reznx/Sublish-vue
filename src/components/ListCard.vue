2d068603ca176982509c0cf53d8bd1bcbc8c6466
Reznx <48242089+Reznx@users.noreply.github.com>
2019-08-29 09:18:57 +0500

    Games & search

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="row">
-    <div class="col s12 m6" >
-      <div class="card" v-for="game in games" :key="game.id">
+    <div class="input-field col s12">
+      <input id="text" type="text" v-model="searchField" />
+      <label for="text">Поиск</label>
+    </div>
+
+    <div class="col s12 m12" v-for="game in filteredGames" :key="game.id">
+      <div class="card">
         <div class="card-image">
           <img :src="game.imageId" />
           <span class="card-title">{{game.title}}</span>
@@ -19,9 +24,25 @@
 
 <script>
 export default {
+  data: () => ({
+    searchField: null
+  }),
   computed: {
     games() {
       return this.$store.getters.getGames;
+    },
+    filteredGames() {
+      let games = this.games;
+      if (this.searchField)
+        games = games.filter(
+          g =>
+            g.title.toLowerCase().indexOf(this.searchField.toLowerCase()) >=
+              0 ||
+            g.description
+              .toLowerCase()
+              .indexOf(this.searchField.toLowerCase()) >= 0
+        );
+      return games;
     }
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,34 @@
 <template>
-  <div class="container">
-    <h1>Главная</h1>
+  <div class="home">
+    <div class="parallax-container">
+      <div class="parallax" ref="parallax">
+        <img src="../assets/images/starwars-home1.jpg" />
+      </div>
+    </div>
+    <div class="section white">
+      <div class="row container">
+        <h2 class="header">Parallax</h2>
+        <p
+          class="grey-text text-darken-3 lighten-3"
+        >Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      </div>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax">
+        <img src="../assets/images/farcry5-home2.jpg" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'home',
-}
+  name: "home",
+  data: () => ({
+    parallax: null
+  }),
+  mounted() {
+    this.parallax = M.Parallax.init(this.$refs.parallax, {});
+  }
+};
 </script>
